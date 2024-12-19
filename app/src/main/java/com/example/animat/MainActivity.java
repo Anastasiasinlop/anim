96550bb9949baf6a3c053fe4b5d5e061a8ba7eef
Настя <vetkav89@gmail.com>
2024-12-19 10:59:46 +0300

    анимация

---
+++ app/src/main/java/com/example/animat/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.animat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+    }
+
+    public void image(View view) {
+        ImageView image = findViewById(R.id.image);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        image.startAnimation(animation);
+
+    }
+
+    public void sled(View view) {
+        Intent intent = new Intent(MainActivity.this, MainActivity3.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/animat/MainActivity3.java
@@ -0,0 +1,42 @@
+package com.example.animat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main3);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void knopka(View view) {
+        ImageView image = findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.scale);
+        image.startAnimation(animation);
+    }
+
+
+    public void cled(View view) {
+        Intent intent = new Intent(MainActivity3.this, MainActivity2.class);
+        startActivity(intent);
+        finish();
+    }
+}
